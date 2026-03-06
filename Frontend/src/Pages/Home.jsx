60277a6a2032a598762dcf215226e738d6ecdfc8
Dheeraj Patidar <patidardheeraj1944@gmail.com>
2026-03-06 20:44:47 +0530

    Update home.jsx

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => (
     <div className="text-center mt-10 text-2xl font-semibold text-gray-800">
-        Welcome to 🗳️ Digital Voting System
+        Welcome to 🗳️ Digital Voting System.
     </div>
 );
 export default Home;
